46876483d2aabacfccb92d3c6110b99efcca3e1e
Furl0w <rainbowphoque@gmail.com>
2018-10-17 17:35:39 +0200

    adding first component

--- src/components/common/content/containers/Content.js
+++ src/components/common/content/containers/Content.js
@@ -1,46 +1,18 @@
 import React from 'react';
+import Visual from '../components/Visual'
 
 export default class Content extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            id: props.content.id,
-            src: props.content.src,
-            type: props.content.type,
-            title: props.content.src,
-            onlyContent: false
+            content: this.props.content
         }
     }
 
     render() {
-        let content
-        switch (this.state.type) {
-            case "img":
-            case "img_url": content = <img src={this.state.src}></img>; break
-            case "video": content = <object data={this.state.src}></object>; break
-            case "web": content = <iframe src={this.state.src}></iframe>; break
-            default: return (
-                <div>
-                    <h4>Unsupported content type</h4>
-                </div>
-            )
-        }
-
-        if (this.state.onlyContent === true) {
-            return (
-                <div>
-                    {content}
-                </div>
-            );
-        }
-        else {
-            return (
-                <div>
-                    {content}
-                    {this.state.id}{this.state.title}{this.state.src}
-                </div>
-            );
-        }
+        return (
+            <Visual content={this.state.content}></Visual>
+        )
     }
 }
