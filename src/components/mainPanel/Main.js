4d5c78e80b11e2fd967d0e87504d51cce501e4c1
Furl0w <rainbowphoque@gmail.com>
2018-10-17 17:18:06 +0200

    second step done

--- src/components/mainPanel/Main.js
+++ src/components/mainPanel/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Main.css';
 import '../../lib/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 import * as contentMapTmp from '../../source/contentMap.json';
+import Content from '../common/content/containers/Content'
 
 export default class Main extends React.Component {
     constructor(props) {
@@ -13,6 +14,10 @@ export default class Main extends React.Component {
     }
 
     render() {
+        let contents = []
+        for (let content in this.state.contentMap) {
+            contents.push(<Content content={this.state.contentMap[content]}></Content>)
+        }
         return (
 
             <div className='container-fluid height-100'>
@@ -20,6 +25,7 @@ export default class Main extends React.Component {
                     <div className='col-md-3 col-lg-3 height-100 vertical-scroll'>
                     </div>
                     <div className='col-md-6 col-lg-6 height-100'>
+                        {contents}
                     </div>
                     <div className='col-md-3 col-lg-3 height-100'>
                     </div>
