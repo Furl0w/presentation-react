d8b470566b03b0dbb5b7c747b0bae2ebc382f77f
CPE <h4dr1en@pm.me>
2018-10-17 17:56:13 +0200

    added browserContentPanel

---
+++ src/components/browseContentPanel/containers/browserContentPanel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Content from '../../common/content/containers/Content'
+
+export default class browserContentPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contentMap: this.props.content
+        }
+    }
+
+    render() {
+        let contents = []
+        for (let content in this.state.contentMap) {
+            contents.push(<Content key={this.state.contentMap[content].id} content={this.state.contentMap[content]}></Content>)
+        }
+        return (<div>{contents}</div>);
+            
+    }
+}
